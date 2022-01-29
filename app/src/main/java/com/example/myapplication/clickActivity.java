04bd1d99fb0eb5eee66446bd47cfb1c86ef93018
vtt232 <66857049+vtt232@users.noreply.github.com>
2022-01-29 10:30:30 +0700

    YOUTUBEEMBED

--- app/src/main/java/com/example/myapplication/clickActivity.java
+++ app/src/main/java/com/example/myapplication/clickActivity.java
@@ -4,17 +4,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 import java.util.ArrayList;
 
 public class clickActivity extends AppCompatActivity {
-    private static String TAG="Success";
+    private static YouTubePlayerView youTubePlayerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Change Activity success", Toast.LENGTH_SHORT).show();
-        Log.d(TAG,"clicked");
+        setContentView(R.layout.click_activity);
+
+        youTubePlayerView=findViewById(R.id.youtube_player);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoID="yyduqrCpKGg";
+                youTubePlayer.loadVideo(videoID,0);
+            }
+        });
     }
 
 
